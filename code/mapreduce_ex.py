8ff6c69daabf91da3bb163d441ed2cdacaa964f7
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-06-07 17:45:15 +0300

    added code from lecture

--- code/mapreduce_ex.py
+++ code/mapreduce_ex.py
@@ -1,8 +1,21 @@
-def mapper_get_ratings(self, _, line):
-    (userID, movieID, rating, timestamp) = line.split('\t')
-    yield rating, 1
+from mrjob.job import MRJob
+from mrjob.step import MRStep
 
-def reducer_count_ratings(self, key, value):
-    yield key, sum(value)
 
-from mrjob.job import
+class RatingsBreakdown(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer=self.reducer_count_ratings)
+        ]
+
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield rating, 1
+
+    def reducer_count_ratings(self, key, value):
+        yield key, sum(value)
+
+
+if __name__ == '__main__':  # if we are executing from cmd - kick this job off
+    RatingsBreakdown.run()
